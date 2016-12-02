31e46e6d24ed6e9e042b08195a6a302aa84c0718
Abhishek Sarkar <aksarkar@mit.edu>
2016-12-02 12:24:19 -0500

    Plot rank correlation in simulation

--- R/rank.R
+++ R/rank.R
@@ -3,17 +3,19 @@ requireNamespace('dplyr')
 
 #' @include theme_nature.R
 
-plot_rank_correlation <- function(correlation_file) {
+plot_rank_correlation <- function(correlation_file, order=TRUE) {
     correlations <- read.table(correlation_file)
-    correlations$V2 <- factor(correlations$V2,
-                              levels=c('WTCCC', 'NARAC1', 'NARAC2', 'EIRA',
-                                       'CANADA', 'BRASS', 'Overall'))
+    if (order) {
+        correlations$V2 <- factor(correlations$V2,
+                                  levels=c('WTCCC', 'NARAC1', 'NARAC2', 'EIRA',
+                                           'CANADA', 'BRASS', 'Overall'))
+    }
     p <- (ggplot(correlations, aes(x=V1, y=V3, color=factor(V2))) +
           geom_line(size=I(.25)) +
           geom_hline(yintercept=0, color="black", size=I(.1), linetype='dashed') +
           scale_x_continuous(limits=c(0, 1e5), expand=c(0, 0), name='Top n SNPs (hold-out absolute z-score)') +
-          scale_y_continuous(limits=c(0, 1), name='Hold-out Pearson correlation') +
-          scale_color_brewer(palette="Dark2", name="Cohort") +
+          scale_y_continuous(name='Hold-out Pearson correlation') +
+          scale_color_brewer(palette="Paired", name="Cohort") +
           theme_nature +
           theme(legend.position="right"))
 
